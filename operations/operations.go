da7cb54bee4d8bc38fd97788e962fc8d7ca1c934
readygo67 <readygo67@yeah.net>
2024-10-26 22:27:21 +0800

    remove chainark.FpExtrator

--- operations/operations.go
+++ operations/operations.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	kzg_bn254 "github.com/consensys/gnark-crypto/ecc/bn254/kzg"
@@ -17,7 +16,6 @@ import (
 	"github.com/consensys/gnark/logger"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
 	"github.com/consensys/gnark/std/recursion/plonk"
-	"github.com/lightec-xyz/chainark"
 	"github.com/lightec-xyz/common/utils"
 	"github.com/rs/zerolog"
 )
@@ -271,47 +269,4 @@ func ReadSrs(size int, srsDir string) (*kzg.SRS, *kzg.SRS, error) {
 		return nil, nil, fmt.Errorf("incorrect srs lagrange size")
 	}
 	return &srs, &srsLagrange, nil
-}
-
-func ExtractFp(ccs constraint.ConstraintSystem, vk native_plonk.VerifyingKey, srsDir string) error {
-	log := logger.Logger().With().Str("function", "extractFp").Logger()
-
-	fpExtractor := chainark.FpExtractor[sw_bn254.ScalarField, sw_bn254.G1Affine, sw_bn254.G2Affine]{
-		Vkey: plonk.PlaceholderVerifyingKey[sw_bn254.ScalarField, sw_bn254.G1Affine, sw_bn254.G2Affine](ccs),
-	}
-	ccsExtractor, err := frontend.Compile(ecc.BN254.ScalarField(), scs.NewBuilder, &fpExtractor)
-	if err != nil {
-		return err
-	}
-	size := ccsExtractor.GetNbConstraints() + ccsExtractor.GetNbPublicVariables()
-
-	srs, lsrs, err := ReadSrs(size, srsDir)
-	if err != nil {
-		return err
-	}
-
-	extractFpStart := time.Now()
-	pkExtractor, _, err := native_plonk.Setup(ccsExtractor, *srs, *lsrs)
-	if err != nil {
-		return err
-	}
-
-	recursiveVkey, err := plonk.ValueOfVerifyingKey[sw_bn254.ScalarField, sw_bn254.G1Affine, sw_bn254.G2Affine](vk)
-	if err != nil {
-		return err
-	}
-	wExt := chainark.FpExtractor[sw_bn254.ScalarField, sw_bn254.G1Affine, sw_bn254.G2Affine]{
-		Vkey: recursiveVkey,
-	}
-	witnessExtractor, err := frontend.NewWitness(&wExt, ecc.BN254.ScalarField())
-	if err != nil {
-		return err
-	}
-	_, err = native_plonk.Prove(ccsExtractor, pkExtractor, witnessExtractor)
-	if err != nil {
-		return err
-	}
-	log.Debug().Dur("took", time.Since(extractFpStart)).Msg("fp extraction done")
-
-	return nil
 }
