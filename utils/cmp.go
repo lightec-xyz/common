855642965222c632897c3bc5ab04a4bb605c44a6
Weiji Guo <weiji.g@gmail.com>
2025-01-24 22:29:39 +0800

    added function to replace Field.Cmp
    which turns out too heavy

---
+++ utils/cmp.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"math"
+	"math/big"
+
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/math/cmp"
+	"github.com/consensys/gnark/std/math/emulated"
+)
+
+/**
+ * a, b should be already strictly reduced, so that we can employ nbSkipLimbs
+ **/
+func ElementsLessEq[T emulated.FieldParams](api frontend.API, a, b emulated.Element[T], nbSkipLimbs ...int) frontend.Variable {
+	var t T
+	if t.BitsPerLimb() != 64 {
+		panic("unsupported number format")
+	}
+
+	maxU64 := big.NewInt(0).SetUint64(math.MaxUint64)
+	cmp := cmp.NewBoundedComparator(api, maxU64, false)
+
+	skip := 0
+	if len(nbSkipLimbs) > 0 {
+		skip = nbSkipLimbs[0]
+	}
+
+	topLimb := int(t.NbLimbs()) - 1 - skip
+	isSofarEq := api.IsZero(api.Sub(a.Limbs[topLimb], b.Limbs[topLimb]))
+	isSofarLess := cmp.IsLess(a.Limbs[topLimb], b.Limbs[topLimb])
+
+	for i := topLimb; i >= 0; i-- {
+		va, vb := a.Limbs[i], b.Limbs[i]
+
+		isThisEq := api.IsZero(api.Sub(va, vb))
+		isThisLess := cmp.IsLess(va, vb)
+
+		isSofarLess = api.Select(isSofarEq, isThisLess, isSofarLess)
+		isSofarEq = api.And(isSofarEq, isThisEq)
+	}
+
+	for i := t.NbLimbs() - 1; i > uint(topLimb); i-- {
+		api.AssertIsEqual(a.Limbs[i], 0)
+		api.AssertIsEqual(b.Limbs[i], 0)
+	}
+
+	return api.Or(isSofarEq, isSofarLess)
+}
