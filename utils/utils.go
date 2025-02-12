c3565bee96c6f539745b0a7fa903ff090511db37
Weiji Guo <weiji.g@gmail.com>
2025-02-12 13:35:05 +0800

    removed LessThan as it is simply wrapping around std lib

--- utils/utils.go
+++ utils/utils.go
@@ -2,13 +2,7 @@ package utils
 
 import (
 	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/std/math/cmp"
 )
-
-// LessThan returns 1 if a < b, 0 otherwise
-func LessThan(api frontend.API, a, b frontend.Variable) frontend.Variable {
-	return cmp.IsLess(api, a, b)
-}
 
 // IsEqual returns 1 if a == b, 0 otherwise
 func IsEqual(api frontend.API, a, b frontend.Variable) frontend.Variable {
