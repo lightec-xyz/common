7f4d93b1a61c91e46d6e3734d1c6e5f326b65be9
Weiji Guo <weiji.g@gmail.com>
2025-02-12 13:00:56 +0800

    revised LessThan implementation

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,13 @@
 package utils
 
-import "github.com/consensys/gnark/frontend"
+import (
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/math/cmp"
+)
 
 // LessThan returns 1 if a < b, 0 otherwise
 func LessThan(api frontend.API, a, b frontend.Variable) frontend.Variable {
-	return api.IsZero(api.Add(api.Cmp(a, b), 1))
+	return cmp.IsLess(api, a, b)
 }
 
 // IsEqual returns 1 if a == b, 0 otherwise
